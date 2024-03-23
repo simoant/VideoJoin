d0109f51e217941b3462011ab5f5279103e1d964
Anton Simonov <simonov.anton@gmail.com>
2024-03-23 21:42:12 +0100

    Initial Commit

---
+++ VideoJoinUITests/VideoJoinUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  VideoJoinUITestsLaunchTests.swift
+//  VideoJoinUITests
+//
+//  Created by Anton Simonov on 23/3/24.
+//
+
+import XCTest
+
+final class VideoJoinUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
